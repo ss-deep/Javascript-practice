7f2630dae685652ee73dc1898f6120cac046b17e
Deepali <deepalisangole@gmail.com>
2022-11-02 19:17:24 -0500

    practice

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function capitalize(str){
+    let char = str[0].toUpperCase();
+    console.log(char+str.slice(1));
+}
+function lastLetter(str){
+    console.log(str.slice(-1));
+}
+lastLetter("abcd");
+capitalize("sangole");
+
+
+console.log(Math.floor(Math.random()*10));
